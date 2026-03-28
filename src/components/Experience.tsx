0593f57ce282d6590769a870f15542ec64d1fae6
jonbroad15 <jonbroad15@gmail.com>
2026-03-28 19:08:33 -0400

    change logo path

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -35,7 +35,7 @@ const Experience = () => {
         'Mentoring junior team members and collaborating with stakeholders across departments.'
       ],
       technologies: ['Python', 'PyTorch', 'Graph Neural Networks', 'LLMs', 'Transformers'],
-      logo: '/logos/sanofi.svg'
+      logo: 'public/logos/sanofi.svg'
     },
     {
       title: 'Data Scientist (Catalyst Rotation Program)',
